3c5e98c55fc70b8474713c3ef83b0bbf217e6e3d
ChrisTienYenWalker <christienyenwalker@gmail.com>
2023-03-03 15:04:21 -0500

    added file reading system

--- Project/src/ProjectPackage/App.java
+++ Project/src/ProjectPackage/App.java
@@ -1,6 +1,38 @@
 package ProjectPackage;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileLocation = getFile();
+        System.out.println(fileLocation);
+
+        String s;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileLocation))) {
+            while ( (s = reader.readLine()) != null) {
+                System.out.println(s);
+               String[] words = s.split("");
+               for(int i = 0; i < words.length; i++){
+               }
+            }
+            reader.close();
+          }catch (FileNotFoundException ex){
+               System.out.println(ex);
+          }
+          catch (IOException ex){
+              System.out.println(ex);
+          }
+    }
+
+    public static String getFile() {
+        Scanner textScan = new Scanner(System.in);
+        System.out.println("Enter File location: ");
+        String fileLocation = textScan.nextLine();
+        textScan.close();
+        return fileLocation.strip();
     }
 }
