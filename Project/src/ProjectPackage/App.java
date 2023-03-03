3a5f8f4db3a7ca1ea47cf82e0cf8b1c768da6c3c
ChrisTienYenWalker <christienyenwalker@gmail.com>
2023-03-03 15:06:52 -0500

    comments

--- Project/src/ProjectPackage/App.java
+++ Project/src/ProjectPackage/App.java
@@ -8,11 +8,17 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        //get the fle location
         String fileLocation = getFile();
         System.out.println(fileLocation);
 
         String s;
+
+        //open file
         try (BufferedReader reader = new BufferedReader(new FileReader(fileLocation))) {
+
+            //read each line indiviually 
             while ( (s = reader.readLine()) != null) {
                 System.out.println(s);
                String[] words = s.split("");
@@ -20,6 +26,8 @@ public class App {
                }
             }
             reader.close();
+
+            //exception handling 
           }catch (FileNotFoundException ex){
                System.out.println(ex);
           }
@@ -29,10 +37,15 @@ public class App {
     }
 
     public static String getFile() {
+
+        //create scanner
         Scanner textScan = new Scanner(System.in);
+
+        //read in file
         System.out.println("Enter File location: ");
         String fileLocation = textScan.nextLine();
         textScan.close();
+
         return fileLocation.strip();
     }
 }
