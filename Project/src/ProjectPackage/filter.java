4f1b238abaa2c6835b6d66e423afd8704c310e93
ChrisTienYenWalker <christienyenwalker@gmail.com>
2023-03-03 21:27:59 -0500

    added abstract class for filter

---
+++ Project/src/ProjectPackage/filter.java
@@ -0,0 +1,8 @@
+package ProjectPackage;
+import java.util.ArrayList;
+abstract public class filter {
+    private ArrayList<String> entries =new ArrayList<String>();
+    public abstract void filter();
+    public abstract void inputs();
+    public abstract void outputs();
+}
