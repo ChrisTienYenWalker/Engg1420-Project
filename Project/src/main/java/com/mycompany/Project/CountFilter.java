a83ebaa7957e13093d91d4757dfaa755f49e5372
CK8008 <116814547+CK8008@users.noreply.github.com>
2023-03-26 16:16:43 -0400

    Update CountFilter.java

--- Project/src/main/java/com/mycompany/Project/CountFilter.java
+++ Project/src/main/java/com/mycompany/Project/CountFilter.java
@@ -1,10 +1,49 @@
 package com.mycompany.Project;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
-public class CountFilter extends Processing_elements {
+public class countFilter
+{
+    private ArrayList<File> entries = null;
+    private String Key;
+    private int Min;
+    private ArrayList<File> subFiles = null;
 
-    //constructor
+    public ArrayList<File> getSubFiles() {
+        return subFiles;
+    }
+
+    public void setSubFiles(ArrayList<File> subFiles) {
+        this.subFiles = subFiles;
+    }
+    
+    public void setKey(String Key){
+        this.Key = Key;
+    }
+    
+    public String getKey(){
+        return Key;
+    }
+    
+    public void setMin(int Min){
+        this.Min = Min;
+    }
+    
+    public int getMin(){
+        return Min;
+    }
+    
+    public void setEntries(ArrayList<File> entries){
+        this.entries = entries;
+    }
+    
+    public ArrayList<File> getEntries(){
+        return entries;
+    }
+    
     public CountFilter(ArrayList<String> inputValue, String pastEntries) {
         //if there's no entries use past entries
 
