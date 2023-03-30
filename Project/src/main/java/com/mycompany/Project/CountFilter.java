f276a526be0e018bffa8ff5e9ed58430ded95005
CK8008 <116814547+CK8008@users.noreply.github.com>
2023-03-29 20:17:54 -0400

    Update CountFilter.java

--- Project/src/main/java/com/mycompany/Project/CountFilter.java
+++ Project/src/main/java/com/mycompany/Project/CountFilter.java
@@ -1,193 +1,139 @@
-// <<<<<<< HEAD
-// // package com.mycompany.Project;
-// =======
-// <<<<<<< HEAD
-// // package com.mycompany.Project;
+package com.mycompany.Project;
 
-// // import java.io.File;
-// // import java.util.ArrayList;
-// // import java.util.Scanner;
-// // import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+import java.util.List;
 
-// // public class CountFilter extends Processing_elements {
-// //     private ArrayList<File> entries = null;
-// //     private String Key;
-// //     private int Min;
-// //     private ArrayList<File> subFiles = null;
+public abstract class CountFilter extends Processing_elements
+{
 
-// // <<<<<<< HEAD
-// //     // constructor
-// //     public CountFilter(ArrayList<String> inputValue, ArrayList<String> pastEntries) {
+    private String key;
+    private int min;
+    private ArrayList<File> pastEntries = new ArrayList<>();
+    private String repoID = null;
+    private String entryID = null;
+    private String path = null;
+    private Boolean local = false;
+    ArrayList <File> fileList = new ArrayList<>();
+    ArrayList <File> outputList = new ArrayList<>();
 
-// // =======
-// // >>>>>>> 5699710a51cf54207880e5361e196c035e0b8fa4
-// //     public ArrayList<File> getSubFiles() {
-// //         return subFiles;
-// //     }
-
-// //     public void setSubFiles(ArrayList<File> subFiles) {
-// //         this.subFiles = subFiles;
-// //     }
-
-// //     public void setKey(String Key) {
-// //         this.Key = Key;
-// //     }
-
-// //     public String getKey() {
-// //         return Key;
-// //     }
-
-// //     public void setMin(int Min) {
-// //         this.Min = Min;
-// //     }
-
-// //     public int getMin() {
-// //         return Min;
-// //     }
-
-// //     public void setEntries(ArrayList<File> entries) {
-// //         this.entries = entries;
-// //     }
-
-// //     public ArrayList<File> getEntries() {
-// //         return entries;
-// //     }
-// // <<<<<<< HEAD
-
-// //     public CountFilter(ArrayList<String> inputValue, String pastEntries) {
-// // =======
-// =======
-// package com.mycompany.Project;
-// >>>>>>> 47bd7562eb94274431cb7893433479090b579002
-
-// // import java.io.File;
-// // import java.util.ArrayList;
-// // import java.util.Scanner;
-// // import java.io.*;
-
-// // public class CountFilter extends Processing_elements {
-// //     private ArrayList<File> entries = null;
-// //     private String Key;
-// //     private int Min;
-// //     private ArrayList<File> subFiles = null;
-
-// <<<<<<< HEAD
-// // <<<<<<< HEAD
-// //     // constructor
-// //     public CountFilter(ArrayList<String> inputValue, ArrayList<String> pastEntries) {
-
-// // =======
-// // >>>>>>> 5699710a51cf54207880e5361e196c035e0b8fa4
-// //     public ArrayList<File> getSubFiles() {
-// //         return subFiles;
-// //     }
-// =======
-//     public ArrayList<File> getSubFiles() {
-//         return subFiles;
-//     }
-// >>>>>>> 47bd7562eb94274431cb7893433479090b579002
-
-// //     public void setSubFiles(ArrayList<File> subFiles) {
-// //         this.subFiles = subFiles;
-// //     }
-
-// //     public void setKey(String Key) {
-// //         this.Key = Key;
-// //     }
-
-// //     public String getKey() {
-// //         return Key;
-// //     }
-
-// //     public void setMin(int Min) {
-// //         this.Min = Min;
-// //     }
-
-// //     public int getMin() {
-// //         return Min;
-// //     }
-
-// //     public void setEntries(ArrayList<File> entries) {
-// //         this.entries = entries;
-// //     }
-
-// <<<<<<< HEAD
-// //     public ArrayList<File> getEntries() {
-// //         return entries;
-// //     }
-// // <<<<<<< HEAD
-
-// //     public CountFilter(ArrayList<String> inputValue, String pastEntries) {
-// // =======
-// =======
-//     public ArrayList<File> getEntries() {
-//         return entries;
-//     }
-// >>>>>>> 18cbb84c323fda8d93b0d42e6c3b48293ebfe0f8
-// >>>>>>> 47bd7562eb94274431cb7893433479090b579002
-    
-// //     public static boolean hasKey(String entry, String Key, int Min){
-// //         try(BufferedReader br = new BufferedReader(new FileReader(entry))){
-// //             String line;
-// //             int count = 0;
-// //             while ((line = br.readLine()) != null){
-// //                 if (line.contains(key)){
-// //                     count++;
-// //                     if(count >= min){
-// //                         return true;
-// //                     }
-// //                 }
-// //             }
-// //         }catch (IOException e){
-// //             e.printStackTrace();
-// //         }
-// //         return false;
-// //     }
-    
-// <<<<<<< HEAD
-// //     public static ArrayList<String> fileProcess (ArrayList<String> entries, String key, int min){
-// //         List<String> result = new ArrayList<String>();
-// =======
-// <<<<<<< HEAD
-// //     public CountFilter(ArrayList<String> entries, String Key, int Min) {
-// // >>>>>>> 5699710a51cf54207880e5361e196c035e0b8fa4
-// //         //if there's no entries use past entries
-// =======
-//     public static ArrayList<String> fileProcess (ArrayList<String> entries, String key, int min){
-//         List<String> result = new ArrayList<String>();
-// >>>>>>> 47bd7562eb94274431cb7893433479090b579002
+    public CountFilter(ArrayList<String> inputValues, ArrayList<String> pastEntries) {
         
-// //         for (int entry = 0; entry < entries.toString().length(); entry++){
-// //             if (isFile(entry)){
-// //                 if (hasKey(entry, key, min)){
-// //                     result.add(entry);
-// //                 }
-// //             }
-// //         }
-// //         return (ArrayList<String>) result;
-// //     }
-// // }
+        int remote = 0;
+        
+        for(String filePath : pastEntries){
+            File newFile = new File(filePath);
+            fileList.add(newFile);
+        }
+
+        for (String text : inputValues) {
+            System.out.println(text);
+  
+            if (text.contains("value") || text.contains("Value")) {
+                key = text.replaceAll("value", "").replaceAll(" ", "").replaceAll(":", "");
+            }
+            if (text.contains("value") || text.contains("value")){
+                min = Integer.parseInt(text.replaceAll("value", "").replaceAll(" ", "").replaceAll(":", ""));
+            }
+            if(text.contains("type") && text.contains("local"))
+                local = true;
+            
+            if(local){
+                if (text.contains("path")) {
+                    path = text.replaceAll("path", "").replaceAll(" ", "").replaceAll(":", "");
+                }
+            }
+            if (text.contains("type") && text.contains("remote")) {
+                remote = 2;
+            }
+            if(remote > 0){
+                if (text.contains("repoId") || text.contains("value")) {
+                    repoID = text.replaceAll("repoId", "").replaceAll(" ", "").replaceAll(":", "");
+                }
+                if (text.contains("entryId") || text.contains("value")) {
+                    entryID = text.replaceAll("entryId", "").replaceAll(" ", "").replaceAll(":", "");
+                }
+                remote--;
+            }
+        }
+        try{
+            getEntriesRemoteFileNames("r-0001d410ba56", 26);
+        }catch(Exception e){
+            // System.out.println(e);
+        }
+
+        try{
+            getEntriesLocal(path);
+        }catch(Exception e){
+            // System.out.println(e);
+        }
+        for(String text: data){   // use data arrayList to 
+            System.out.println(text);
+        }
+    }
+
+    public static boolean ifFile(String filePath) {
+        File file = new File(filePath);
+        return file.isFile();
+    }
+
+    public static boolean ifDirectory(String filePath) {
+        File file = new File(filePath);
+        return file.isDirectory();
+    }
+
+    public void opertaions() {
+        Boolean totalKey = true;
     
-// <<<<<<< HEAD
-// //     public CountFilter(ArrayList<String> entries, String Key, int Min) {
-// // >>>>>>> 5699710a51cf54207880e5361e196c035e0b8fa4
-// //         //if there's no entries use past entries
-// =======
-//     public CountFilter(ArrayList<String> entries, String Key, int Min) {
-// >>>>>>> 18cbb84c323fda8d93b0d42e6c3b48293ebfe0f8
-// >>>>>>> 47bd7562eb94274431cb7893433479090b579002
+        if (local == false) {
+            try {
+                for (int i = 0; i < fileList.size(); i++) {
+                    try (BufferedReader br = new BufferedReader(new FileReader(fileList.get(i)))) {
+                        String line;
+                        int count = 0;
+                        while ((line = br.readLine()) != null) {
+                            if (line.contains(key)) {
+                                count++;
+                                if (count < min) {
+                                    totalKey = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if (totalKey == true) {
+                            outputList.add(fileList.get(i));
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        } else {
+            if (ifDirectory(path)) {
+                System.out.println("Please use valid file path");
+            } else if (ifFile(path)) {
+                int count = 0;
+                for (String line : data) {
+                    if (line.contains(key)) {
+                        count++;
+                        if (count < min) {
+                            totalKey = false;
+                            break;
+                        }
+                    }
+                }
+                if (totalKey == true) {
+                    outputList.add(new File(path));
+                }
+            } else {
+                System.out.println("File path not found");
+            }
+        }
+    }
 
-// //     }
+    public void outputs(){ 
+       
+    }
 
-// //     // define these functions
-// <<<<<<< HEAD
-// //     public void opertaions() {
-// =======
-// //     public void operations() {
-// >>>>>>> 47bd7562eb94274431cb7893433479090b579002
-
-// //     };
-
-// //     public void outputs() {
-// //     };
-// // }
+}
