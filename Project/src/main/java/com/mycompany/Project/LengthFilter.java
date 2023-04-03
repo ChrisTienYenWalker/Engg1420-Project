adec86428f3ecec6321990e964029faa9d8bb472
msalaz03 <116814238+msalaz03@users.noreply.github.com>
2023-04-02 20:09:37 -0400

    Final LengthFilter Changes

--- Project/src/main/java/com/mycompany/Project/LengthFilter.java
+++ Project/src/main/java/com/mycompany/Project/LengthFilter.java
@@ -2,21 +2,16 @@ package com.mycompany.Project;
 
 import java.util.ArrayList;
 import java.io.*;
-
-
-
 import java.util.function.Consumer;
-
 import com.laserfiche.api.client.model.AccessKey;
 import com.laserfiche.repository.api.RepositoryApiClient;
 import com.laserfiche.repository.api.RepositoryApiClientImpl;
 
 public class LengthFilter extends Processing_elements{
+    //Declarlation of Variables + Getters / Setters
     private long length = 0;
     private String Operator;
-    private ArrayList<File>filteredFiles = new ArrayList<>();
-    private ArrayList<File>subFiles = null;
-
+    
     public void setOperator(String operator){
         this.Operator = operator;
     }
@@ -34,15 +29,12 @@ public class LengthFilter extends Processing_elements{
     }
 
     
-
-
-    
-
+    //Constructor
     public LengthFilter(ArrayList<String> inputs, ArrayList<String> entries){
         String tempstr = "";
+        //Extract Operators & Length Values.
         for (String text : inputs) {
             System.out.println(text);
-
 
             if (text.contains("Length")){
                 tempstr = "Length";
@@ -55,39 +47,33 @@ public class LengthFilter extends Processing_elements{
             if ((text.contains("value") || text.contains("Value")) && tempstr.equals("Length")) {
                 this.length = Long.parseLong((text.replaceAll("value", "").replaceAll(" ", "").replaceAll(":", ""))); 
             }
+
             if ((text.contains("value") || text.contains("Value")) && tempstr.equals("Operator")) {
                 this.Operator = text.replaceAll("value", "").replaceAll(" ", "").replaceAll(":" , ""); 
             }
         }
         
-
+        //Couldnt extract required variables.
         if(Operator == null || length == 0){
             System.out.println("Parameters couldn't be found in JSON, check formatting");
         }
+
+        //add this file into inputs arrayList
         else
         {
         inputs.addAll(entries);
         loopEntries(inputs);
         }
 
-        for (String text : data) {
-            //System.out.println(text);
-        }
+        
+        // for (String text : data) {
+        //    System.out.println(text);
+        // }
        
          //OUTPUTS
          System.out.println("EXTRACTED CONTENT");
          System.out.println(getOperator());
          System.out.println(getLength());
-
-
-       
-
-
-
-              
-
-
-
     }
 
 
@@ -95,13 +81,7 @@ public class LengthFilter extends Processing_elements{
     @Override
     public void operations()
     {
-         //local check
-        //file check
-        //swtich
-
-        //remote check(else)
-        //directory check
-        //switch
+        //LOCAL CASE
         if(local){
         File subFile = new File(path);
         if (subFile.isFile()){
@@ -180,7 +160,7 @@ public class LengthFilter extends Processing_elements{
                     break;
                 }
         }
-        
+        //LengthFilter only deals with directories
         else if(!subFile.isFile())
         {
             System.out.println("File does not exist or is directory.");
@@ -188,7 +168,7 @@ public class LengthFilter extends Processing_elements{
 
         }
 
-
+        //Remote Case
         else if (!local)
         {
             if (isRemoteDIR(entryID)){
@@ -269,20 +249,60 @@ public class LengthFilter extends Processing_elements{
                     }
                 
             }
-
-
+        }
         }
 
-       
-        }
+    //Get File Size for Remote
+    protected long getRemoteFileSize(String entryID) {
+        String servicePrincipalKey = "x0BmysMxlH_XfLoc69Kk";
+        String accessKeyBase64 = "ewoJImN1c3RvbWVySWQiOiAiMTQwMTM1OTIzOCIsCgkiY2xpZW50SWQiOiAiOGFkZTZjNTctZDIxNS00ZmYyLThkOTctOTE1YjRiYWUyZWIzIiwKCSJkb21haW4iOiAibGFzZXJmaWNoZS5jYSIsCgkiandrIjogewoJCSJrdHkiOiAiRUMiLAoJCSJjcnYiOiAiUC0yNTYiLAoJCSJ1c2UiOiAic2lnIiwKCQkia2lkIjogImNCeWdXYnh6YU9jRHZVcUdBU1RfcURTY0plcWw3aU9Ya19SZVFleUpiTzQiLAoJCSJ4IjogIjZNSXNuODRLanFtMEpTUmhmS2tHUTRzbGhkcldCbVNMWk9nMW5oWjhubFkiLAoJCSJ5IjogIlpkZ1M1YWIxdU0yaVdaWHVpdmpBc2VacC11LWlJUlc4MjFwZWhENVJ5bUkiLAoJCSJkIjogIldjN091cDFYV3FudjlEVFVzQWZIYmxGTDFqU3UwRWJRY3g0LXNqbG0xRmMiLAoJCSJpYXQiOiAxNjc3Mjk3NTU0Cgl9Cn0=";
+        AccessKey accessKey = AccessKey.createFromBase64EncodedAccessKey(accessKeyBase64);
+        RepositoryApiClient client = RepositoryApiClientImpl.createFromAccessKey(
+                servicePrincipalKey, accessKey);
+        // create a new file and store the remote file in a new local file  
 
-        private long getRemoteFileSize(String entryID) {
-        return 0;
+        // delete old file
+        File deleteFile = new File("Project\\remoteFile.txt");
+
+        // create new file
+        final String FILE_NAME = "Project\\remoteFile.txt";
+        Consumer<InputStream> consumer = inputStream -> {
+            File exportedFile = new File(FILE_NAME);
+            try (FileOutputStream outputStream = new FileOutputStream(exportedFile)) {
+                byte[] buffer = new byte[1024];
+                while (true) {
+                    int length = inputStream.read(buffer);
+                    if (length == -1) {
+                        break;
+                    }
+                    outputStream.write(buffer, 0, length);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    inputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        // get the file details
+        client.getEntriesClient()
+                .exportDocument(this.repoID, Integer.parseInt(entryID), null, consumer)
+                .join();
+        long length = deleteFile.length();
+        deleteFile.delete();
+        return length;
     }
 
-        private void entryFail(){
-            System.out.println("Entry did not meet size requirements.");
-        }
+    //Simple Function used in Remote switch case
+    private void entryFail()
+    {
+        System.out.println("Entry did not meet size requirements.");
+    }
 
 }
+
 
